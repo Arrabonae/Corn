84c27fd9bc5050f1e523e790c8187bf7724f3fa8
Graystone <wexylum@gmail.com>
2019-11-06 10:20:07 +0100

    define model structure

--- production/app.py
+++ production/app.py
@@ -12,10 +12,12 @@ from flask import Flask, request, jsonify
 ##Load the pretrained network
 
 #Load the Structure
+model = tf.keras.models.model_from_file() #Still need to be defined
 
 #Load the weights
-
+model.load_weights() #Still need to be defined
 
 ##Create the Flask API
 
 #Define the Flask application
+app = Flask(__name__)
