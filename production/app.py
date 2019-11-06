d756f6fa5875db33227f06a487cabad21bf9e5fb
Graystone <wexylum@gmail.com>
2019-11-06 10:18:24 +0100

    new folder strucutre, dependecies are loaded now

---
+++ production/app.py
@@ -0,0 +1,21 @@
+
+##import dependecies
+import os
+import requests
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from flask import Flask, request, jsonify
+
+
+##Load the pretrained network
+
+#Load the Structure
+
+#Load the weights
+
+
+##Create the Flask API
+
+#Define the Flask application
