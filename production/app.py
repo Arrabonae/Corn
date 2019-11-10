0f7f3549a4e911d5aec889418abc2a35e0db7311
Graystone <wexylum@gmail.com>
2019-11-10 20:39:37 +0800

    updated strucutre

--- production/app.py
+++ production/app.py
@@ -30,75 +30,76 @@ app = Flask(__name__)
 #Load the data from upload
 @app.route("/api/v1/<string:data>", methods=["POST"])
 
-#Variables
-n_steps = 10 # How many days to predict
+def predict(data):
+    def read_it(data):
+        inputs2 = pd.read_csv("uploads/" + data)
+        cols = inputs2.columns
+        return inputs2, cols
 
-#Format the Data
-df = read_it(data)
-df2 = scale_it(inputs2, cols)
-dataset = reshape(df2)
-X = split(dataset, n_steps)
+    #MinMaxScaler
+    def scale_it(inputs2, cols):
+        scaler = MinMaxScaler()
+        inputs3 = pd.DataFrame(scaler.fit_transform(inputs2))
+        inputs3.columns = cols
+        return inputs3
 
-#Variables cont. 
-n_features = X.shape[2]
-
-def read_it(data):
-    inputs2 = pd.read_csv("uploads/" + data)
-    cols = inputs2.columns
-    return inputs2, cols
-
-#MinMaxScaler
-def scale_it(inputs2, cols):
-    scaler = MinMaxScaler()
-    inputs3 = pd.DataFrame(scaler.fit_transform(inputs2))
-    inputs3.columns = cols
-    return inputs3
-
-#Reshape for 
-def reshape (dataset): #,y_seq): This variable was only required during development phase of the modeling, not required for production 
-    #Get the column headers
-    cols2 = np.array(dataset.columns)
-    #cols = np.delete(cols, np.where(cols == y_seq))
-    seq_list = []
-    
-    #split the dataset into vectors and convert into a 4D tensor
-    for i in range(len(cols2)):
-        seq = np.array(dataset[cols2[i]])
-        #Convert to [row, columns] structure
-        seq = seq.reshape((len(seq), 1))
-        seq_list.append(seq)
+    #Reshape for 
+    def reshape (dataset): #,y_seq): This variable was only required during development phase of the modeling, not required for production 
+        #Get the column headers
+        cols2 = np.array(dataset.columns)
+        #cols = np.delete(cols, np.where(cols == y_seq))
+        seq_list = []
         
-    #This part was only required to develop the model, under production there is no 'y' variable
-    #out = np.array(dataset[y_seq])
-    #out = out.reshape((len(out), 1))
-    #seq_list.append(out)
-    
-    dataset_shaped = np.hstack((seq_list))
-    #horizontally stack columns
-    return dataset_shaped
+        #split the dataset into vectors and convert into a 4D tensor
+        for i in range(len(cols2)):
+            seq = np.array(dataset[cols2[i]])
+            #Convert to [row, columns] structure
+            seq = seq.reshape((len(seq), 1))
+            seq_list.append(seq)
+            
+        #This part was only required to develop the model, under production there is no 'y' variable
+        #out = np.array(dataset[y_seq])
+        #out = out.reshape((len(out), 1))
+        #seq_list.append(out)
+        
+        dataset_shaped = np.hstack((seq_list))
+        #horizontally stack columns
+        return dataset_shaped
 
-    # split a multivariate sequence into samples
-def split(sequences, n_steps):
-    X = list()  # updated to production ## y = list(),
-    for i in range(len(sequences)):
-        # find the end of this pattern
-        end_ix = i + n_steps
-        # check if we are beyond the dataset
-        if end_ix > len(sequences):
-            break
-        # gather input and output parts of the pattern
-        seq_x = sequences[i:end_ix, :-1] # seq_y= , sequences[end_ix-1, -1] (Updated to production)
-        X.append(seq_x)
-        #y.append(seq_y)
-    return np.array(X) #, array(y)
+        # split a multivariate sequence into samples
+    def split(sequences, n_steps):
+        X = list()  # updated for production ## y = list(),
+        for i in range(len(sequences)):
+            # find the end of this pattern
+            end_ix = i + n_steps
+            # check if we are beyond the dataset
+            if end_ix > len(sequences):
+                break
+            # gather input and output parts of the pattern
+            seq_x = sequences[i:end_ix, :-1] # seq_y= , sequences[end_ix-1, -1] (Updated to production)
+            X.append(seq_x)
+            #y.append(seq_y)
+        return np.array(X) #, array(y)
 
-#Perform predictions with pre-trained model
-prediction = model.predict(X)
-scaler = MinMaxScaler()
-scaled_pred = scaler.inverse_transform(prediction)
 
-#Return the prediction to the user
-jsonify(scaled_pred)
+    n_steps = 10 # How many days to predict, required for the model
+
+    #Format the Data
+    inputs2, cols = read_it(data)
+    df2 = scale_it(inputs2, cols)
+    dataset = reshape(df2)
+    X = split(dataset, n_steps)
+
+    #Variables -> this variable is required for the model
+    n_features = X.shape[2]
+
+    #Perform predictions with pre-trained model
+    prediction = model.predict(X, n_steps, n_features)
+    scaler = MinMaxScaler()
+    scaled_pred = scaler.inverse_transform(prediction)
+
+    #Return the prediction to the user
+    return jsonify(scaled_pred)
 
 #Start the Flask application
 app.run(port=5000, debug=False)
