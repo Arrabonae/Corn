63a9f9fc485d30fd3ebbfa3b8544f3816d99a734
Graystone <wexylum@gmail.com>
2019-12-15 11:47:28 +0800

    tidy things up

--- production/app.py
+++ production/app.py
@@ -16,7 +16,6 @@ import datetime
 app = Flask(__name__)
 
 # Create a directory in a known location to save files to.
-#app.config['UPLOAD'] = '/Users/snowwhite/Documents/Github/Project Osaka/production/uploads/'
 app.config['UPLOAD'] = os.path.join(os.getcwd(),'production/uploads')
 #app.config['UPLOAD'] = os.path.join(os.path.realpath('app.py'),'/uploads')
 
@@ -36,7 +35,7 @@ def upload_form():
 #Define the Prediction function, 
 @app.route('/forward', methods=['GET', 'POST'])
 def predict():  
-    #Read the data - in the proper format
+    #Read the data - in the proper format, loosing rows with empy cells
     def load_input_file():
         inputs2 = pd.read_csv(os.path.join(app.config['UPLOAD'], 'data.csv'), parse_dates=['Date'])
         inputs2.set_index('Date', inplace=True) #Date added
@@ -44,14 +43,16 @@ def predict():
         cols = inputs2.columns
         return inputs2, cols
 
+    #This function is required as the Weights are saved from a trained Network where the data was Scaled. 
+    #Hence if we want to make predictions and meaningful ones, we need to scale the predicted data back, buy using the 'Last' Column as a reference point
     def back_scale(y_train, y_hat):
         scaler = MinMaxScaler()
-        y_train_scaled = pd.DataFrame(scaler.fit_transform(y_train.reshape(-1,1)))
+        y_train_scaled = pd.DataFrame(scaler.fit_transform(y_train.reshape(-1,1))) #only requied to fit the scaler
         y_hat_scaled = pd.DataFrame(scaler.inverse_transform(y_hat.reshape(-1,1)))
         return y_hat_scaled
 
     #Reshape the date for LSTM use, As we use more than one Features (LSTM Network - usually takes 1 feature to work with - the Tensors need to reshaped
-    def reshape (dataset, y_seq): 
+    def reshape_to_tensor(dataset, y_seq): 
         #Get the column headers
         cols2 = np.array(dataset.columns)
         cols2 = np.delete(cols2, np.where(cols2 == y_seq))
@@ -68,12 +69,11 @@ def predict():
         out = out.reshape((len(out), 1))
         seq_list.append(out)
         
-        dataset_shaped = np.hstack((seq_list))
-        #horizontally stack columns
+        dataset_shaped = np.hstack((seq_list)) #horizontally stack columns
         return dataset_shaped
 
     # split a multivariate sequence into samples/batches, This is an LSTM related slip to make sure that the Tensors have enough dimensions for the Network to process
-    def split(sequences, n_steps):
+    def split_to_timestamps(sequences, n_steps):
         X, y = list(), list()
         for i in range(len(sequences)):
             # find the end of this pattern
@@ -92,19 +92,39 @@ def predict():
     #We have to make some shortcuts, as the LSTM network cannot handle the fact that there are no features in the future. 
     #Hence, the features are engineered as a way to project them (using mean) to the future, and therefore providing enough datapoints for the Network to work with.  
     def forecast(n_prediction, model,dataset_shaped,n_steps):
-        look_back = 120 #so using 60 days wort of data to calculate the next mean
+        look_back = 120 #so using 120 days worth of data to calculate the next mean
         prediction_list = dataset_shaped[-look_back:][::-1]
         result = []
     
         for _ in range(n_prediction):
             z = prediction_list[-look_back:]
-            x, y = split(z, n_steps)
+            x, y = split_to_timestamps(z, n_steps)
             out = model.predict(x)[0][0]
             features_mean= z.mean(axis=0)  #Projecting the features to the future
             prediction_list = np.vstack((prediction_list,features_mean))
             result.append(out)
         return result
-    
+
+    #rendering the data into Graph to push it out into the result.html
+    def build_graph(final_scaled, inputs2, n_prediction):
+        dataset = pd.DataFrame(final_scaled)
+        dataset.index = [x for x in pd.date_range(inputs2.index.values[0] + pd.offsets.Day(1),periods=n_prediction)]
+        img = BytesIO()
+        plt.figure(figsize=(15,8))
+        plt.xlabel('Dates')
+        plt.ylabel('Closing Prices')
+        plt.title('Continuous Corn Futures Historical and Forecast Prices')
+        plt.grid(True)
+        plt.plot(inputs2['Last'], 'blue', label='Historical Data')
+        plt.plot(dataset, 'red', label='Forecast')
+        plt.legend()
+        plt.savefig(img, format='png')
+        img.seek(0)
+        graph_url = base64.b64encode(img.getvalue()).decode()
+        plt.close()
+        return 'data:image/png;base64,{}'.format(graph_url)
+
+
 #All the magic will happen from here
 
 #Variables
@@ -113,14 +133,9 @@ def predict():
  
     n_steps = 5 #How many days to use to predict, required for the model
     n_prediction = 30 #How many day to predict ahead in the future
-    
     inputs2, cols = load_input_file() # First of all we read the data in in the right format
-    #input3 = scale_it(scaler, inputs2, cols) # the data what we read, needs to be scaled with MinMaxScaler
-    dataset_shaped = reshape(inputs2, 'Last') #'Last' #format the scaled data for LSTM use (part 1, where are formatting the columns into arrays)
-    #np.save('shaped', dataset_shaped)
-    X, y = split(dataset_shaped, n_steps) #Also need to format the Scaled data into 4D tensors
-    #np.save('X_train', X)
-    #np.save('y_train', y)
+    dataset_shaped = reshape_to_tensor(inputs2, 'Last') #'Last' #format the scaled data for LSTM use (part 1, where are formatting the columns into arrays)
+    X, y = split_to_timestamps(dataset_shaped, n_steps) #Also need to format the Scaled data into 4D tensors
     n_features = X.shape[2] # This wariable is required for the model, gives the feature dimension
     
  #Define the LSTM architecture, the weights are loaded seperatelly, but I've decided to keep the architecture part of the main code for more transparency
@@ -139,37 +154,17 @@ def predict():
         tf.keras.layers.Dense(1),
         ])
     #Load the pre-trained weights into the model
-    #LSTM_model.load_weights('/Users/snowwhite/Documents/Github/Project Osaka/production/weights_3.h5')
     LSTM_model.load_weights(os.path.join(os.getcwd(),'production/weights_3.h5'))
     adam=tf.keras.optimizers.Adam(lr=0.003711, epsilon=None, amsgrad=True, decay=0)
-    LSTM_model.compile(loss ='mean_squared_error', optimizer=adam, metrics=['accuracy'])   
-
-    final_forecast = forecast(n_prediction, LSTM_model,dataset_shaped,n_steps)
-    final_scaled = back_scale(y, np.array(final_forecast))
-    #final_scaled = pd.DataFrame(final_scaled)
-    def build_graph(final_scaled, inputs2, n_prediction):
-        #dates = np.array([datetime.datetime(inputs2.datetimeIndex(inputs2.index[0]).year,inputs2.datetimeIndex(inputs2.index[0]).month, inputs2.datetimeIndex(inputs2.index[0]).day, i, 0) for i in range(24)])
-        dataset = pd.DataFrame(final_scaled)
-        dataset.index = [x for x in pd.date_range(inputs2.index.values[0] + pd.offsets.Day(1),periods=n_prediction)]
-        img = BytesIO()
-        plt.figure(figsize=(15,8))
-        plt.xlabel('Dates')
-        plt.ylabel('Closing Prices')
-        plt.title('Continuous Corn Futures Historical and Forecast Prices')
-        plt.grid(True)
-        plt.plot(inputs2['Last'], 'blue', label='Historical Data')
-        plt.plot(dataset, 'red', label='Forecast')
-        plt.legend()
-        plt.savefig(img, format='png')
-        img.seek(0)
-        graph_url = base64.b64encode(img.getvalue()).decode()
-        plt.close()
-        return 'data:image/png;base64,{}'.format(graph_url)
+    LSTM_model.compile(loss ='mean_squared_error', optimizer=adam, metrics=['accuracy'])
     
-    plot_url = build_graph(final_scaled, inputs2, n_prediction)
+    
+    scaled_forecast = forecast(n_prediction, LSTM_model,dataset_shaped,n_steps)
+    predictions = back_scale(y, np.array(scaled_forecast)) #Scale the data to make meaningful datapoints
+    plot_url = build_graph(predictions, inputs2, n_prediction) #render the graph
 
     #Return the prediction to the user
-    return render_template('result.html', plot_url = plot_url, tables=[final_scaled.to_html(classes='data')], titles = final_scaled.columns.values)
+    return render_template('result.html', plot_url = plot_url, tables=[predictions.to_html(classes='data', header=False)], titles = ['Forecast'])
 
 #Start the Flask application
 if __name__ == '__main__':
